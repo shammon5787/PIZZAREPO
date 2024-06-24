78e5e18314ef7ffed30d6eb2c324f94f39f9167a
shammon5787 <sadam.bsit@gmail.com>
2024-06-24 16:01:02 +0500

    additions

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,26 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoClose } from "react-icons/io5";
 import CardItem from './CardItem';
+import { useSelector } from 'react-redux';
+import { FaCartShopping } from "react-icons/fa6";
 
 const Card = () => {
+
+    const cartItem = useSelector((state)=>state.cart.cart)
+
+    const [activeCard, setactiveCard] = useState(true)
+
+    // const totalQty = cartItem.reduce((totalQty , items)=>totalQty, items + items.qty);
+
     return (
         <>
-            <div className='bg-white fixed top-0 right-0 h-full w-full lg:w-[300px] p-2'>
+            <div className={`bg-white fixed top-0 right-0 h-full w-full lg:w-[300px] p-2 ${activeCard ? "translate-x-0" : "translate-x-full"} transition-all duration-500 z-50`}>
                 <div className='bg-transparent flex items-center justify-between'>
                     <span className='bg-transparent text-slate-700 font-semibold'>Your Order</span>
-                    <IoClose className='bg-transparent font-semibold text-2xl cursor-pointer animate-pulse hover:animate-none' />
+                    <IoClose onClick={()=>setactiveCard(!activeCard)} className='bg-transparent font-semibold text-2xl cursor-pointer animate-pulse hover:animate-none transition-all duration-500' />
                 </div>
-
-                <CardItem />
-                <CardItem />
-                <CardItem />
+                {
+                   cartItem.length > 0 ? cartItem.map((food)=>(
+                        <CardItem key={food.id} id = {food.id} name = {food.name} image = {food.image} desc = {food.desc}qty = {food.qty} price={food.price}  />
+                    )) : <h2 className='bg-transparent text-3xl font-semibold shadow-inner'>No Item Selected</h2>
+                }
 
                 <div className='bg-transparent absolute bottom-0'>
-                    <h1 className='bg-transparent text-slate-700'>Total Items: </h1>
+                    <h1 className='bg-transparent text-slate-700'>Total Items:  </h1>
                     <h1 className='bg-transparent text-slate-700'>Total Price: </h1>
                     <button className='mb-3 bg-blue-700 p-1 rounded-md text-white font-semibold w-[92vw] lg:w-[21vw]'>Check Out</button>
                 </div>
             </div>
+
+            <FaCartShopping onClick={()=>setactiveCard(!activeCard)} className={`fixed bottom-4 right-10 cursor-pointer text-3xl rounded-full  ${cartItem.length > 0 ? "animate-bounce" : " animate-none"} items-center justify-center p-2 bg-green-700 hover:animate-none `} />
         </>
     )
 }
--- src/Components/CardItem.jsx
+++ src/Components/CardItem.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import img from '../Food/1.avif'
 import { FaMinus, FaPlus } from "react-icons/fa6";
 import { MdDelete } from "react-icons/md";
+import { useDispatch } from 'react-redux';
+import { remove } from '../Store/CardSlice';
 
-const CardItem = () => {
+const CardItem = ({id, name, image,price, desc, qty}) => {
+    const dispatch = useDispatch()
+
+
     return (
         <div className='bg-transparent flex shadow-md gap-3 p-2'>
-            <MdDelete className='absolute right-2 bg-transparent cursor-pointer' />
-            <img className='rounded-full w-[50px] h-[50px] ' src={img} alt="" />
+            <MdDelete onClick={()=>dispatch(remove({id}))} className='absolute right-2 bg-transparent cursor-pointer' />
+            <img className='rounded-full w-[50px] h-[50px] ' src={image} alt="" />
             <div className='bg-transparent leading-4 items-center justify-between'>
-                <h1 className='bg-transparent'> Onion Pizza</h1>
+                <h1 className='bg-transparent'> {name}</h1>
                 <div className='bg-transparent flex items-center justify-between'>
-                    <span className='bg-transparent'>23</span>
+                    <span className='bg-transparent'>{price}</span>
                     <div className='bg-transparent flex items-center absolute right-2'>
                         <FaPlus className='bg-transparent cursor-pointer hover:bg-green-700 hover:sc110 duration-500 rounded-full hover:text-white' />
-                        <span className='bg-transparent'>1</span>
+                        <span className='bg-transparent'>{qty}</span>
                         <FaMinus className='bg-transparent cursor-pointer hover:bg-green-700 hover:sc110 duration-500 rounded-full hover:text-white' />
                     </div>
                 </div>
